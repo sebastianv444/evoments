4b1da76f1791fc3af2c27bdbf709adb7efc1cf03
sebastian <sebastian12verositi@gmail.com>
2025-04-05 14:23:43 +0200

    Fix page construction

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,67 @@ function App() {
 
   return (
     <>
-      <h2>Nuestro sitio web</h2>
-      <h1>PRÓXIMAMENTE...</h1>
+      <h1 style={{ color: "#9D4EDD" }}>EVOMENTS</h1>
+      <h2>PRÓXIMAMENTE...</h2>
       <p>
-        Nuestro proyecto llamado evoments sería una página web para crear
-        eventos y gestionar eventos. Desarrollar una plataforma web donde se
-        puedan crear, gestionar y asistir a eventos dentro de una zona creada
-        ficticiamente por nosotros. La aplicación estará dirigida tanto a
-        creadores de eventos como a clientes que deseen asistir a ellos.
-      </p><br /><br /><br />
-      <img
-        src="/pre-landing/construccion.png"
-        alt="construccion"
-        width={230}
-      /><br /><br />
-      <h3>Desarrolladores:</h3>
-      <p>Iván de la Torre Moreno</p>
-      <p>Sebastián Moreno Villalobos</p>
+        Somos Evoments, una aplicación innovadora donde podrás crear, gestionar
+        y administrar tus eventos de forma sencilla y eficiente. Nuestro
+        objetivo es conectar a organizadores con usuarios que buscan
+        experiencias únicas, permitiendo que estos puedan adquirir entradas de
+        forma rápida, interactiva y sin complicaciones. Somos un equipo de
+        jóvenes ambiciosos con ganas de revolucionar la forma en que se viven y
+        se comparten los eventos. Además, si tienes una idea para un evento pero
+        no sabes cómo publicarlo o promocionarlo, Evoments está aquí para
+        ayudarte. Te apoyamos desde el primer paso, asegurándonos de que tu
+        evento llegue al público adecuado y tenga el impacto que deseas.
+      </p>
+      <br />
+      <br />
+      <br />
+      <img src="/pre-landing/construccion.png" alt="construccion" width={230} />
+      <br />
+      <br />
+      <h2>Desarrolladores:</h2>
+      <div style={{display: "flex", justifyContent: "center", 
+        alignItems: "center", gap: "8px"}}>
+        <p>👨‍💻 Iván de la Torre Moreno</p>
+        <a href="https://www.instagram.com/" target="_blank">
+          <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{marginTop: "8px"}}
+          >
+            <path
+              d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2Zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5a4.25 4.25 0 0 0 4.25-4.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7ZM17.5 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2Z"
+              fill="currentColor"
+            />
+          </svg>
+        </a>
+      </div>
+      
+      
+      <div style={{display: "flex", justifyContent: "center", 
+        alignItems: "center", gap: "8px"}}>
+        <p>👨‍💻 Sebastián Moreno Villalobos</p>
+        <a href="https://www.instagram.com/" target="_blank">
+          <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{marginTop: "8px"}}
+          >
+            <path
+              d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2Zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5a4.25 4.25 0 0 0 4.25-4.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7ZM17.5 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2Z"
+              fill="currentColor"
+            />
+          </svg>
+        </a>
+      </div>
     </>
   );
 }
