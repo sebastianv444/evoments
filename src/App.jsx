dd4378f1797c94f225dc7274b0788d414baadf7f
sebastian <sebastian12verositi@gmail.com>
2025-04-05 13:07:32 +0200

    Page in construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Nuestro sitio web</h2>
+      <h1>PRÓXIMAMENTE...</h1>
+      <p>
+        Nuestro proyecto llamado evoments sería una página web para crear
+        eventos y gestionar eventos. Desarrollar una plataforma web donde se
+        puedan crear, gestionar y asistir a eventos dentro de una zona creada
+        ficticiamente por nosotros. La aplicación estará dirigida tanto a
+        creadores de eventos como a clientes que deseen asistir a ellos.
+      </p><br /><br /><br />
+      <img
+        src="../public/pre-landing/construccion.png"
+        alt="construccion"
+        width={230}
+      /><br /><br />
+      <h3>Desarrolladores:</h3>
+      <p>Iván de la Torre Moreno</p>
+      <p>Sebastián Moreno Villalobos</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
