fc14b0e91e486862e6497cd3a2f69fe7593dda8c
sebastian <sebastian12verositi@gmail.com>
2025-04-24 13:04:37 +0200

    añadiendo avance para la visualizacion en las tutorias

---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,15 @@
+import HomeSection1 from "./Section1";
+import HomeSection2 from "./Section2";
+import HomeSection3 from "./Section3";
+
+function Home() {
+  return (
+    <>
+      <HomeSection1 />
+      <HomeSection2 />
+      <HomeSection3 />
+    </>
+  );
+}
+
+export default Home;
